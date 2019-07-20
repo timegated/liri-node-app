f99cd22062cd63038b236a9efb35508f96683d32
Dan Beccaria <beccdaniel@gmail.com>
2019-07-19 22:07:15 -0400

    adding files

---
+++ keys.js
@@ -0,0 +1,7 @@
+console.log('this is loaded')
+
+exports.Spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+};
+
---
+++ liri.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+const keys = require("./keys.js");
+
+const spotify = new Spotify(keys.spotify);
+
+
